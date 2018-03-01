ebeb21040c002000235d0e617a4b0ee18feeb890
Donovan Hare <contact@donovanha.re>
2018-03-01 09:41:20 +0000

    Ditto

--- src/components/general/TitleButton.vue
+++ src/components/general/TitleButton.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <br>
+        <a class="button is-dark is-medium animated bounceIn" :href="link" @click.once="nextStep" v-smooth-scroll><slot></slot></a>            
     </div>
 </template>
 
